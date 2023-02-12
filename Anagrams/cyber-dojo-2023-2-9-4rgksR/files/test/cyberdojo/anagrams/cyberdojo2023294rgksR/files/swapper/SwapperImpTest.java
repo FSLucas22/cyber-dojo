5d831050f4ae4d0148beefe79472b853e1b4400f
Hywenklis <hywenklis@hotmail.com>
2023-02-12 16:30:26 -0300

    Refatoração para melhorar a legibilidade e performance do código

--- Anagrams/cyber-dojo-2023-2-9-4rgksR/files/test/cyberdojo/anagrams/cyberdojo2023294rgksR/files/swapper/SwapperImpTest.java
+++ Anagrams/cyber-dojo-2023-2-9-4rgksR/files/test/cyberdojo/anagrams/cyberdojo2023294rgksR/files/swapper/SwapperImpTest.java
@@ -1,7 +1,9 @@
-package swapper;
+package test.swapper;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import swapper.Swapper;
+import swapper.SwapperImp;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
