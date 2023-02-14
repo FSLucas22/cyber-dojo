3827d6c1272678409498c4bf44d4bca4633902cf
Lucas Ferreira dos Santos <fs.lucas22@gmail.com>
2023-02-14 12:13:02 -0300

    Revert "Novo exercício de fizzbuzz"
    This reverts commit 81de5d47acf1ca5db44699eb32eec43d4303d8e3.
    
    	deleted:    Fizzbuzz/cyber-dojo-2023-2-14-clmmEr/README.md
    	deleted:    Fizzbuzz/cyber-dojo-2023-2-14-clmmEr/events.json
    	deleted:    Fizzbuzz/cyber-dojo-2023-2-14-clmmEr/files/Converter.java
    	deleted:    Fizzbuzz/cyber-dojo-2023-2-14-clmmEr/files/Counter.java
    	deleted:    Fizzbuzz/cyber-dojo-2023-2-14-clmmEr/files/CounterTest.java
    	deleted:    Fizzbuzz/cyber-dojo-2023-2-14-clmmEr/files/Display.java
    	deleted:    Fizzbuzz/cyber-dojo-2023-2-14-clmmEr/files/FizzBuzz.java
    	deleted:    Fizzbuzz/cyber-dojo-2023-2-14-clmmEr/files/FizzBuzzTest.java
    	deleted:    Fizzbuzz/cyber-dojo-2023-2-14-clmmEr/files/cyber-dojo.sh
    	deleted:    Fizzbuzz/cyber-dojo-2023-2-14-clmmEr/files/readme.txt
    	deleted:    Fizzbuzz/cyber-dojo-2023-2-14-clmmEr/manifest.json
    	deleted:    Fizzbuzz/cyber-dojo-2023-2-14-clmmEr/options.json
    	deleted:    Fizzbuzz/cyber-dojo-2023-2-14-clmmEr/status
    	deleted:    Fizzbuzz/cyber-dojo-2023-2-14-clmmEr/stderr
    	deleted:    Fizzbuzz/cyber-dojo-2023-2-14-clmmEr/stdout
    	deleted:    Fizzbuzz/cyber-dojo-2023-2-14-clmmEr/truncations.json

--- Fizzbuzz/cyber-dojo-2023-2-14-clmmEr/files/Converter.java
+++
@@ -1,4 +0,0 @@
-@FunctionalInterface
-public interface Converter {
-    String convert(int value);
-}
--- Fizzbuzz/cyber-dojo-2023-2-14-clmmEr/files/Display.java
+++
@@ -1,4 +0,0 @@
-@FunctionalInterface
-public interface Display {
-    public void show(String value);
-}
